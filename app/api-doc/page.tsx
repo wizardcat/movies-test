640ab69b45f1d98a7df6e20dd8cd1fd8b3eb99b9
Roman Havrylko <groman2000@gmail.com>
2024-05-31 19:34:30 +0300

    fix: pages fix

--- app/api-doc/page.tsx
+++ app/api-doc/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import spec from "@/app/api/v1/api-v1.json";
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import Movies from '../../components/Movies/Movies';
 
 export default function LoginPage() {
