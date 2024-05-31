d87d249d3367d441704778f050492b6ea43ad265
Roman Havrylko <groman2000@gmail.com>
2024-05-31 20:34:39 +0300

    fix: comment swagger

--- app/api-doc/page.tsx
+++ app/api-doc/page.tsx
@@ -1,13 +1,12 @@
 /* eslint-disable */
 "use client";
-import spec from "@/app/api/v1/api-v1.json";
-import SwaggerUI from "swagger-ui-react";
+// import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 
 export default function Page() {
   return (
     <section className="container">
-      <SwaggerUI spec={spec} />
+      {/* <SwaggerUI spec={spec} /> */}
     </section>
   );
 }
