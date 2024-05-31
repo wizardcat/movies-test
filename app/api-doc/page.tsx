3b0fde4aab87c73c1c73c8a3c6b39559f2722641
Roman Havrylko <groman2000@gmail.com>
2024-05-31 23:58:06 +0300

    fix: swag

--- app/api-doc/page.tsx
+++ app/api-doc/page.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable */
 "use client";
-import spec from "@/app/api/v1/api-v1.json";
+import v1 from "@/app/api/v1/api-v1.json";
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 
 export default function Page() {
+  const spec = v1;
   return (
     <section className="container">
       <SwaggerUI spec={spec} />
