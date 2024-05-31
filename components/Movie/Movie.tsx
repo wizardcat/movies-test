0729bb9908b3301f2cccc90f451503e4d17163f9
Ihor Kubrak <ih.kubrak@gmail.com>
2024-06-01 00:00:02 +0200

    tmp

--- components/Movie/Movie.tsx
+++ components/Movie/Movie.tsx
@@ -40,7 +40,20 @@ export default function Movie({ id }: {id?: string}) {
 
   const handleMovie = () => {
     const movieDataCreation = {title, publishingYear: Number(publishingYear), posterFile: poster};
-    const movieDataEditing = {...movieDataCreation, id: id as string};
+
+    const dataURLtoFile = (dataUrl: any) => {
+      const arr = dataUrl.split(',');
+      const mime = arr[0].match(/:(.*?);/)[1];
+      const bstr = atob(arr[1]);
+      let n = bstr.length;
+      const u8arr = new Uint8Array(n);
+      while (n--) {
+        u8arr[n] = bstr.charCodeAt(n);
+      }
+      return new File([u8arr], movieData?.poster || "", {type: "mime"});
+    }
+    
+    const movieDataEditing = {...movieDataCreation, id: id as string, posterFile: dataURLtoFile(imageFromId?.image)};
     if (id) {
       mutationForEditing(movieDataEditing);
     } else {
