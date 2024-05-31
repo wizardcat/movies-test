11ddf24633c84ede0e9e877af0bcf3fdc16006ee
Roman Havrylko <groman2000@gmail.com>
2024-06-01 01:45:23 +0300

    fix: api link

--- hooks/api/queries/useGetPoster.ts
+++ hooks/api/queries/useGetPoster.ts
@@ -1,5 +1,6 @@
-import { useQuery, UseQueryOptions } from '@tanstack/react-query';
-import axios from 'axios';
+import { config } from "@/app/common/config";
+import { useQuery, UseQueryOptions } from "@tanstack/react-query";
+import axios from "axios";
 
 interface ImageResponse {
   success: boolean;
@@ -7,15 +8,16 @@ interface ImageResponse {
 }
 
 const fetchImage = async (fileName: string): Promise<ImageResponse> => {
-  const response = await axios.get(`/api/v1/images?fileName=${fileName}`);
+  const bucketUri = config.AWS_S3.bucketUri;
+  const response = await axios.get(`${bucketUri}/images?fileName=${fileName}`);
   return response.data;
 };
 
 export const useGetPoster = (fileName: string, options?: UseQueryOptions<ImageResponse, Error>) => {
   return useQuery<ImageResponse, Error>({
-    queryKey: ['image', fileName],
+    queryKey: ["image", fileName],
     queryFn: () => fetchImage(fileName),
     enabled: !!fileName,
     ...options,
-    });
+  });
 };
