6a5f8dbcf6fc6b8955dea46ca2c4443ae8de7ee9
Roman Havrylko <groman2000@gmail.com>
2024-06-01 00:32:53 +0300

    fix: fix2

--- app/api-doc/page.tsx
+++ app/api-doc/page.tsx
@@ -1,10 +1,12 @@
 "use client";
-import SwaggerUI from "swagger-ui-react";
+// import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 import v1 from "./api-v1.json";
 
 export default function Page() {
   const spec = v1;
   
-  return <section className="container">{!spec && <SwaggerUI spec={spec} />}</section>;
+  return <section className="container">
+    {/* <SwaggerUI spec={spec} /> */}
+  </section>;
 }
--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const nextConfig = {
   env: {
     DATABASE_URL: process.env.DATABASE_URL ?? "",
     // AWS_S3_REGION: process.env.AWS_S3_REGION ?? "",
-    AWS_S3_ACCESS_KEY_ID: process.env.AWS_S3_ACCESS_KEY_ID ?? "",
-    AWS_S3_SECRET_ACCESS_KEY: process.env.AWS_S3_SECRET_ACCESS_KEY ?? "",
+    AWS_S3_ACCESS_KEY_ID: process.env.ACCESS_KEY_ID ?? "",
+    AWS_S3_SECRET_ACCESS_KEY: process.env.SECRET_ACCESS_KEY ?? "",
     // AWS_S3_BUCKET_NAME: process.env.AWS_S3_BUCKET_NAME ?? "",
     // AWS_S3_BUCKET_URI: process.env.AWS_S3_BUCKET_URI ?? "",
   },
