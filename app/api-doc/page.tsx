22ef57090868463fc6107107e57088b7b071b481
Roman Havrylko <groman2000@gmail.com>
2024-06-01 00:17:32 +0300

    fix: page swagger

--- app/api-doc/page.tsx
+++ app/api-doc/page.tsx
@@ -1,14 +1,15 @@
-/* eslint-disable */
 "use client";
+import { config } from "@/app/common/config";
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 import v1 from "./api-v1.json";
 
 export default function Page() {
   const spec = v1;
+  
   return (
     <section className="container">
-      <SwaggerUI spec={spec} />
+      {!config.isProduction && <SwaggerUI spec={spec} />}
     </section>
   );
 }
