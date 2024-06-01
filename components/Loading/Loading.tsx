1e16a8d5edf5e8672d82e976180ff503f49adf6c
Ihor Kubrak <ih.kubrak@gmail.com>
2024-06-01 03:45:02 +0200

    Loading component shrinked

--- components/Loading/Loading.tsx
+++ components/Loading/Loading.tsx
@@ -1,27 +1,24 @@
 import React, { useState, useEffect } from 'react';
 
 const Loading = () => {
-  const [loadingText, setLoadingText] = useState('Loading');
-  const [dotCount, setDotCount] = useState(1);
-  const maxDots = 3;
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setDotCount(prevDotCount => {
-        const newDotCount = prevDotCount < maxDots ? prevDotCount + 1 : 1;
-        return newDotCount;
-      });
-    }, 300);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  useEffect(() => {
-    let dots = '.'.repeat(dotCount);
-    setLoadingText(`Loading${dots}`);
-  }, [dotCount]);
-
-  return <h2>{loadingText}</h2>;
-};
+    const [loadingText, setLoadingText] = useState('Loading');
+    const [dotCount, setDotCount] = useState(1);
+    const maxDots = 3;
+  
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setDotCount(prevDotCount => (prevDotCount < maxDots ? prevDotCount + 1 : 1));
+      }, 300);
+  
+      return () => clearInterval(interval);
+    }, []);
+  
+    useEffect(() => {
+      const dots = '.'.repeat(dotCount);
+      setLoadingText(`Loading${dots}`);
+    }, [dotCount]);
+  
+    return (<h2>{loadingText}</h2>);
+  };
 
 export default Loading;
