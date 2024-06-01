08690779ad35109fa12a680fd0955fb4cf35c4a4
Roman Havrylko <groman2000@gmail.com>
2024-06-01 12:59:22 +0300

    fix: change to dynamic swagger loading

--- app/api-doc/page.tsx
+++ app/api-doc/page.tsx
@@ -1,8 +1,10 @@
 "use client";
-import SwaggerUI from "swagger-ui-react";
+// import SwaggerUI from "swagger-ui-react";
+import dynamic from "next/dynamic";
 import "swagger-ui-react/swagger-ui.css";
 import v1 from "./api-v1.json";
 
+const SwaggerUI = dynamic(() => import("swagger-ui-react"), { ssr: false });
 export default function Page() {
   const spec = v1||{};
   console.log("spec", spec);
