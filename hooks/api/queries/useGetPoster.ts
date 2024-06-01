c34d83cca4c67911d86f04c4106c5a51b191c0ae
Ihor Kubrak <ih.kubrak@gmail.com>
2024-06-01 02:23:21 +0200

    try

--- hooks/api/queries/useGetPoster.ts
+++ hooks/api/queries/useGetPoster.ts
@@ -9,22 +9,10 @@ interface ImageResponse {
 
 const fetchImage = async (fileName: string): Promise<ImageResponse> => {
   const bucketUri = config.AWS_S3.bucketUri;
-  try {
-    const response = await axios.get(`${bucketUri}/images/${fileName}`, {
-      headers: {
-        'Accept': 'image/jpeg'
-      }
-    });
-    const corsHeader = response.headers['access-control-allow-origin'];
-    if (!corsHeader) {
-      throw new Error("CORS header not present");
-    }
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching image:", error);
-    throw error;
-  }
+  const response = await axios.get(`${bucketUri}/images/${fileName}`);
+  return response.data;
 };
+
 export const useGetPoster = (fileName: string, options?: UseQueryOptions<ImageResponse, Error>) => {
   return useQuery<ImageResponse, Error>({
     queryKey: ["image", fileName],
